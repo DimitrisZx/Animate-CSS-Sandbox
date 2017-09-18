75b409635710b1d22924871781cf543f9c457303
Dimitris Zarachanis <dimitriszarahanis@gmail.com>
2017-09-18 04:14:25 +0300

    working javascript!

--- js/animate.js
+++ js/animate.js
@@ -1,6 +1,10 @@
-function toggleClass(){
-  var animateClass = document.getElementByTagName('option').value;
-  console.log(animateClass);
-}
 
-toggleClass();
+var animatedText = document.getElementById('animateMe');
+
+function changeClass(){
+  var newClass = document.getElementById('selection').value;
+  animatedText.className = "";
+  animatedText.classList.add("animated");
+  animatedText.classList.add(newClass);
+  console.log(animatedText.classList);
+}
